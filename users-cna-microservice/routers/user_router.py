f1c3608020233ae15a55eaf97c0741b5b578ba63
sachinsom93 <sachinsom507@gmail.com>
2024-08-21 22:26:48 +0530

    Add ENV vars to dockerfiles

--- users-cna-microservice/routers/user_router.py
+++ users-cna-microservice/routers/user_router.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 
 from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 
 from db.dals.user_dal import UserDAL
 from db.models.user import User
@@ -8,22 +9,31 @@ from dependencies import get_user_dal
 
 router = APIRouter()
 
+# Pydantic model for the response
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+    mobile: int
+
+    class Config:
+        orm_mode = True
 
 @router.post("/users")
 async def create_user(name: str, email: str, mobile: str, user_dal: UserDAL = Depends(get_user_dal)):
-    print("name: " + name)
     return await user_dal.create_user(name, email, mobile)
-
 
 @router.put("/users/{user_id}")
 async def update_user(user_id: int, name: Optional[str] = None, email: Optional[str] = None, mobile: Optional[str] = None,
                       user_dal: UserDAL = Depends(get_user_dal)):
     return await user_dal.update_user(user_id, name, email, mobile)
 
-@router.get("/users/{user_id}")
+@router.get("/users/{user_id}", response_model=UserResponse)
 async def get_user(user_id: int, user_dal: UserDAL = Depends(get_user_dal)):
-    return await user_dal.get_user(user_id)
+    user = await user_dal.get_user(user_id)
+    return user
 
-@router.get("/users")
-async def get_all_users(user_dal: UserDAL = Depends(get_user_dal)) -> List[User]:
-    return await user_dal.get_all_users()
+@router.get("/users", response_model=List[UserResponse])
+async def get_all_users(user_dal: UserDAL = Depends(get_user_dal)):
+    users = await user_dal.get_all_users()
+    return users
